c724e3f3fdde4b81d667f4bafacefb2f0f95e8ae
Agus Rustandi <agus.rusty3@gmail.com>
2019-06-21 11:02:06 +0700

    Initial Commit

---
+++ synema/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  synema
+//
+//  Created by syndromme on 21/06/19.
+//  Copyright © 2019 syndromme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ synemaTests/synemaTests.swift
@@ -0,0 +1,34 @@
+//
+//  synemaTests.swift
+//  synemaTests
+//
+//  Created by syndromme on 21/06/19.
+//  Copyright © 2019 syndromme. All rights reserved.
+//
+
+import XCTest
+@testable import synema
+
+class synemaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
